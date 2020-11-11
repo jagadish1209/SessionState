909a35a075acfbe3230e631dfd734413637d04f7
JAGADISH DESINENI <jagadish1209@live.com>
2020-11-11 11:53:34 +0530

    .net core session demo sample

---
+++ SessionState/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SessionState.Models;
+
+namespace SessionState.Controllers
+{
+    public class HomeController : Controller
+    {
+        const string SessionName = "_Name";
+        const string SessionAge = "_Age";
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString(SessionName, "Jagadish");
+            HttpContext.Session.SetInt32(SessionAge, 32);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Name = HttpContext.Session.GetString(SessionName);
+            ViewBag.Age = HttpContext.Session.GetInt32(SessionAge);
+            ViewData["Message"] = "Asp.Net Core !!!.";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    }
+}
